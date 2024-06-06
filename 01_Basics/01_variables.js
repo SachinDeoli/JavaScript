fc0f86d78e25865517c1b01f9f5fba07fa5ac47b
Sachin-Deoli <55172814+oye-bittu@users.noreply.github.com>
2024-06-06 10:10:41 +0000

    rename of files

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const id = 10;
+let name = "Sachin";
+var isMale = true;
+
+address = "sector-46";
+
+let var5;
+
+
+// we can define a variable in 3 different ways using const, let, var, and with no keyword.
+// const keyword -: value cannot be changed if defined once,if tried changing it will give runtime error.
+// let keyword -: used to define the a vairable , for efficient coding weshould use let.
+// var keyword-: used to define the a vairable, it was used earlier, now for efficient coding practise we use let not var keywoed
+         // Variables declared by let are only available inside the block where they’re defined.
+         // Variables declared by var are available throughout the function in which they’re declared.
+// var4 ="hi" -: we can define a variable like this too but it is also not a good coding practise.
+
+console.table([id,name, isMale,address, var5]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+// types of datatype in javascript
+
+// number
+// string
+// boolean
+// null
+// undefined
+
+let a= 10;
+let b = "sachin"
+let c = false;
+let d = null;
+let e = undefined;
+
+console.table([typeof a, typeof b,typeof c,typeof d,typeof e])
