41b4d8e44ba1aee3e20e7bdf2260da711e364fac
Sachin-Deoli <55172814+oye-bittu@users.noreply.github.com>
2024-06-06 10:25:40 +0000

    conversion of datratypes

---
+++ 01_Basics/03_conversionOfDatatypes.js
@@ -0,0 +1,46 @@
+let c = 33;
+let d  = "abc";
+let e = false;
+let f = null;
+let g = undefined;
+
+
+let c1 = Number(c);
+let d1  = Number(d);
+let e1 = Number(e);;
+let f1 = Number(f);;
+let g1 = Number(g);;
+console.table([typeof c1,typeof d1,typeof e1,typeof f1,typeof g1 ])
+console.table([ c1, d1, e1, f1, g1 ])
+
+// ---------------------------------------------------------------------------
+// let c = 33;
+// let d  = "abc";
+// let e = false;
+// let f = null;
+// let g = undefined;
+
+
+// let c1 = String(c);
+// let d1  = String(d);
+// let e1 = String(e);;
+// let f1 = String(f);;
+// let g1 = String(g);;
+// console.table([typeof c1,typeof d1,typeof e1,typeof f1,typeof g1 ])
+// console.table([ c1, d1, e1, f1, g1 ])
+
+// ---------------------------------------------------------------------------
+// let c = 33;
+// let d  = "abc";
+// let e = false;
+// let f = null;
+// let g = undefined;
+
+
+// let c1 = Boolean(c);
+// let d1  = Boolean(d);
+// let e1 = Boolean(e);;
+// let f1 = Boolean(f);;
+// let g1 = Boolean(g);;
+// console.table([typeof c1,typeof d1,typeof e1,typeof f1,typeof g1 ])
+// console.table([ c1, d1, e1, f1, g1 ])
